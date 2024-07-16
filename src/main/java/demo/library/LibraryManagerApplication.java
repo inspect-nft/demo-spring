4245d58549dcfdfdc149527d431d7f7917366dd3
Madhawa Gunasekara <madhawag@Madhawas-MacBook-Pro.local>
2024-07-16 21:55:29 +0800

    Configure H2 in-memory DB with properties

--- src/main/java/demo/library/LibraryManagerApplication.java
+++ src/main/java/demo/library/LibraryManagerApplication.java
@@ -1,4 +1,4 @@
-package demo.library.libraryManager;
+package demo.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/demo/library/LibraryManagerApplicationTests.java
+++ src/test/java/demo/library/LibraryManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package demo.library.libraryManager;
+package demo.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
